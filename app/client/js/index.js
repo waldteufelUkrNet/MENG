f1e6f85ce47b379b1d8bb23e00e01ec4862d05d9
waldteufelUkrNet <waldteufel@ukr.net>
2021-07-21 20:22:43 +0300

    example mongodb REST

--- app/client/js/index.js
+++ app/client/js/index.js
@@ -1,1 +1,185 @@
 "use strict"
+////////////////////////////////////////////////////////////////////////////////
+/* ↓↓↓ variables declaration ↓↓↓ */
+  const form      = document.forms.userForm,
+        inputId   = form.querySelector('input[type="hidden"][name="id"]'),
+        inputName = form.querySelector('input[name="name"]'),
+        inputAge  = form.querySelector('input[name="age"]'),
+        btnSave   = form.querySelector('button[type="submit"]'),
+        btnReset  = form.querySelector('button[type="button"]'),
+        table     = document.getElementById('clientsTable');
+/* ↑↑↑ /variables declaration ↑↑↑ */
+////////////////////////////////////////////////////////////////////////////////
+/* ↓↓↓ workflow ↓↓↓ */
+  getAllUsers();
+/* ↑↑↑ /workflow ↑↑↑ */
+////////////////////////////////////////////////////////////////////////////////
+/* ↓↓↓ event handlers ↓↓↓ */
+  // save/resave item
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+    submitForm();
+  });
+
+  // reset form
+  btnReset.addEventListener('click', function(event){
+    resetForm();
+  });
+
+  table.addEventListener('click', function(event){
+    // download item for saving
+    if ( event.target.closest('button[data-role="change"]') ) {
+      let id = event.target.closest('tr').getAttribute('id');
+      downloadCurrentUser(id);
+    }
+    // delete item
+    if ( event.target.closest('button[data-role="delete"]') ) {
+      let id = event.target.closest('tr').getAttribute('id');
+      deleteCurrentUser(id);
+    }
+  });
+/* ↑↑↑ /event handlers ↑↑↑ */
+////////////////////////////////////////////////////////////////////////////////
+/* ↓↓↓ functions declaration ↓↓↓ */
+  async function getAllUsers(){
+    let response = await fetch('api/users',{
+      method: 'GET',
+      headers: {"Accept":"application/json"}
+    });
+    if (response.ok) {
+      let users = await response.json();
+      users.forEach( user => {
+        addUserLine(user);
+      });
+
+    } else {
+      console.log('проблеми з підключенням до бази даних');
+    }
+  }
+
+  async function deleteCurrentUser(id) {
+    let response = await fetch('api/users/' + id, {
+      method: 'DELETE',
+      headers: {"Accept":"application/json"},
+    });
+    if (response.ok) {
+      let user = await response.json();
+      removeUserLine(user._id);
+    }
+  }
+
+  async function downloadCurrentUser(id) {
+    let result = await fetch('api/users/' + id, {
+      method: 'GET',
+      headers: {'Accept': 'application/json'}
+    });
+    if (result.ok) {
+      let user = await result.json();
+      inputId.value = user._id;
+      inputName.value = user.name;
+      inputAge.value = user.age;
+    }
+  }
+
+  async function addUser(body){
+    let response = await fetch('api/users', {
+      method: 'POST',
+      headers: {
+        "Accept": "application/json",
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(body)
+    });
+    if (response.ok) {
+      let user = await response.json();
+      addUserLine(user);
+    }
+  }
+
+  async function changeUser(body){
+    let request = await fetch('api/users/' + body.id, {
+      method: 'PUT',
+      headers: {'Accept': 'application/json', 'Content-type': 'application/json'},
+      body: JSON.stringify(body)
+    });
+    if (request.ok) {
+      let user = await request.json();
+      let oldLine = document.querySelector('tr[id="' + user.id + '"]');
+      let newLine = '\
+                      <tr id="' + user.id + '">\
+                        <td>' + user.id + '</td>\
+                        <td>' + user.name + '</td>\
+                        <td>' + user.age + '</td>\
+                        <td>\
+                            <button class="userForm__button" type="button" data-role="change"> Змінити </button>\
+                            <button class="userForm__button" type="button" data-role="delete"> Видалити </button>\
+                        </td>\
+                      </tr>\
+                    ';
+      oldLine.insertAdjacentHTML('afterEnd', newLine);
+      oldLine.remove();
+      resetForm();
+    }
+  }
+
+  function submitForm() {
+    let id   = inputId.value   || 0,
+        name = inputName.value || 0,
+        age  = inputAge.value  || 0;
+
+    if (!name || !age) {
+      alert('заповніть форму!');
+      return;
+    }
+
+    if (!id) {
+      addUser({age: age, name: name});
+      resetForm();
+    } else {
+      changeUser({id: id, age: age, name: name});
+    }
+  }
+
+  function addUserLine(user) {
+    let htmlString = '\
+                      <tr id="' + user._id + '">\
+                        <td>' + user._id + '</td>\
+                        <td>' + user.name + '</td>\
+                        <td>' + user.age + '</td>\
+                        <td>\
+                            <button class="userForm__button" type="button" data-role="change"> Змінити </button>\
+                            <button class="userForm__button" type="button" data-role="delete"> Видалити </button>\
+                        </td>\
+                      </tr>\
+                     ';
+
+    document.querySelector('#clientsTable tbody').insertAdjacentHTML('beforeEnd', htmlString);
+  }
+
+  function removeUserLine(id){
+    document.querySelector('tr[id="' + id + '"]').remove();
+  }
+
+  function resetForm() {
+    let formInputs = form.querySelectorAll('input');
+    formInputs.forEach(item => {
+      item.value = '';
+    });
+  }
+/* ↑↑↑ /functions declaration ↑↑↑ */
+////////////////////////////////////////////////////////////////////////////////
+
+// db.users.insertMany([
+//   {name: "Василь", age: 12},
+//   {name: "Степан", age: 25},
+//   {name: "Микола", age: 12},
+//   {name: "Петро", age: 59},
+//   {name: "Клим", age: 36},
+//   {name: "Юхим", age: 31},
+//   {name: "Богдан", age: 15},
+//   {name: "Михайло", age: 11},
+//   {name: "Гнат", age: 47},
+//   {name: "Тиміш", age: 14},
+//   {name: "Юрій", age: 19},
+//   {name: "Антін", age: 20}
+// ]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 'use strict'
-
-// на майбутнє: мініфікація картинок у проміжну теку (з changed), build уже через неї
 ////////////////////////////////////////////////////////////////////////////////
 /* ↓↓↓ VARIABLES ↓↓↓ */
   const { src,
@@ -21,15 +19,6 @@
         rename  = require('gulp-rename'),
         scss    = require('gulp-sass')(require('sass')),
         uglify  = require('gulp-uglify-es').default;
-
-  // gulp-babel               // ES6 -> ES5
-  // gulp-cache               // бібліотека кешування
-  // gulp-changed             // контроль за змінами у файлах - пропускає потік далі, тільки якщо були зміни у файлі
-  // gulp-concat-css          // склеювання css-файлів
-  // gulp-css-purge           // видалення дублюючого коду css
-  // gulp-cssnano / gulp-csso // мініфікація css-файлів
-  // gulp-load-plugins        // щоб не оголошувати кожну змінну, застосовується для плагінів із префіксом gulp-
-  // gulp-sourcemaps          //
 /* ↑↑↑ /VARIABLES ↑↑↑ */
 ////////////////////////////////////////////////////////////////////////////////
 /* ↓↓↓ TASKS ↓↓↓ */
@@ -67,10 +56,7 @@
               message : 'Error: <%= error.message %>',
               title   : 'SASS error'
             }))
-           .pipe (autopre ({
-                            overrideBrowserslist: ['last 10 version'],
-                            grid: 'autoplace'
-                            }) )
+           .pipe (autopre ({overrideBrowserslist: ['last 10 version'], grid: 'autoplace'}) )
            .on('error', notify.onError({
               message : 'Error: <%= error.message %>',
               title   : 'Autoprefixer error'
@@ -105,69 +91,80 @@
   exports.startWatch = startWatch;
 
   exports.default = series(convertSCSS, convertJS, convertPug, parallel(startBrowserSync, startWatch));
-
-
-
-// exports.default = parallel(browserSync, watching);
-
-// function build(){
-//   return src([...], {base:'app'}).pipe( dest(dest) )
-// }
-// exports
-
-// function callback() {
-//   console.log('trololo')
-// }
-
-
-
-
-
-
-// чищення каталогу dist
-// gulp.task('clean', function(done) {
-//   return del('dist');
-//   done();
-// });
-
-// обробка зображень
-// gulp.task('img', function() {
-//   return gulp.src('app/img/**/*')
-//     // .pipe(cache(imagemin({
-//     //   interlaced  : true,
-//     //   progressive : true,
-//     //   svgoPlugins : [{removeViewBox: false}],
-//     //   use         : [pngquant()]
-//     // })))
-//     .pipe(gulp.dest('dist/img'));
-// });
-
-// очистка кешу
-// gulp.task('clear', function () {
-//     return cache.clearAll();
-// })
-
-// // перенесення файлів з каталогу app в dist
-// gulp.task('build', gulp.series(['clean', 'img'], function(done) {
-
-//   // pug -> html
-//   gulp.src('app/index.pug').pipe(pug({pretty : false})).pipe(gulp.dest('dist/'));
-//   gulp.src('app/books/**/*.pug').pipe(pug({pretty : false})).pipe(gulp.dest('dist/books/'));
-
-//   // fonts
-//   gulp.src('app/fonts/**/*').pipe(gulp.dest('dist/fonts'));
-
-//   // js
-//   // gulp.src('app/js/**/*').pipe(uglify()).pipe(gulp.dest('dist/js/'));
-//   gulp.src('app/js/**/*').pipe(gulp.dest('dist/js/'));
-
-//   // css
-//   gulp.src('app/css/**/*').pipe(csso()).pipe(gulp.dest('dist/css/'));
-
-//   // img
-//   gulp.src('app/books/**/*.jpg').pipe(gulp.dest('dist/books/'));
-
-//   done();
-// }));
 /* ↑↑↑ /TASKS ↑↑↑ */
+////////////////////////////////////////////////////////////////////////////////
+/* ↓↓↓ ЩО ДОРОБИТИ ↓↓↓ */
+  // на майбутнє: мініфікація картинок у проміжну теку (з changed), build уже через неї
+
+  // gulp-babel               // ES6 -> ES5
+  // gulp-cache               // бібліотека кешування
+  // gulp-changed             // контроль за змінами у файлах - пропускає потік далі, тільки якщо були зміни у файлі
+  // gulp-concat-css          // склеювання css-файлів
+  // gulp-css-purge           // видалення дублюючого коду css
+  // gulp-cssnano / gulp-csso // мініфікація css-файлів
+  // gulp-load-plugins        // щоб не оголошувати кожну змінну, застосовується для плагінів із префіксом gulp-
+  // gulp-sourcemaps          //
+
+  // задачі
+  // pug, scss, js: index, pages, libs
+  // img
+  // build
+  // fonts
+  // reset
+
+  // function build(){
+  //   return src([...], {base:'app'}).pipe( dest(dest) )
+  // }
+  // exports
+
+  // function callback() {
+  //   console.log('trololo')
+  // }
+
+  // чищення каталогу dist
+  // gulp.task('clean', function(done) {
+  //   return del('dist');
+  //   done();
+  // });
+
+  // обробка зображень
+  // gulp.task('img', function() {
+  //   return gulp.src('app/img/**/*')
+  //     // .pipe(cache(imagemin({
+  //     //   interlaced  : true,
+  //     //   progressive : true,
+  //     //   svgoPlugins : [{removeViewBox: false}],
+  //     //   use         : [pngquant()]
+  //     // })))
+  //     .pipe(gulp.dest('dist/img'));
+  // });
+
+  // очистка кешу
+  // gulp.task('clear', function () {
+  //     return cache.clearAll();
+  // })
+
+  // // перенесення файлів з каталогу app в dist
+  // gulp.task('build', gulp.series(['clean', 'img'], function(done) {
+
+  //   // pug -> html
+  //   gulp.src('app/index.pug').pipe(pug({pretty : false})).pipe(gulp.dest('dist/'));
+  //   gulp.src('app/books/**/*.pug').pipe(pug({pretty : false})).pipe(gulp.dest('dist/books/'));
+
+  //   // fonts
+  //   gulp.src('app/fonts/**/*').pipe(gulp.dest('dist/fonts'));
+
+  //   // js
+  //   // gulp.src('app/js/**/*').pipe(uglify()).pipe(gulp.dest('dist/js/'));
+  //   gulp.src('app/js/**/*').pipe(gulp.dest('dist/js/'));
+
+  //   // css
+  //   gulp.src('app/css/**/*').pipe(csso()).pipe(gulp.dest('dist/css/'));
+
+  //   // img
+  //   gulp.src('app/books/**/*.jpg').pipe(gulp.dest('dist/books/'));
+
+  //   done();
+  // }));
+/* ↑↑↑ /ЩО ДОРОБИТИ ↑↑↑ */
 ////////////////////////////////////////////////////////////////////////////////
